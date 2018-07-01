367787da60709441022b4ebb19996e3a065934bf
Rodrigo Acosta <rodrigo.acosta@smartvalor.com>
2018-07-01 20:08:05 -0300

    added basic tests to mint the SmartValorToken

---
+++ contracts/test/SmartValorTokenTests.js
@@ -0,0 +1,54 @@
+const MyContract = artifacts.require("SmartValorToken");
+
+contract('MyContract', async (accounts) => {
+  let smartValorToken;
+
+  before(async () => {
+     smartValorToken = await MyContract.deployed();
+  });
+
+  /**
+   * mint some tokens to a non owner account
+   */
+  it("should mint tokens to a non owner account.", async () => {
+    let owner = accounts[0]; //this account with mint
+    let accountOne = accounts[1]; //to this account
+    let amount = 10000000; //amount of tokens to mint
+
+    // account should have no balance
+    let balanceBefore = await smartValorToken.balanceOf.call(accounts[1]);
+    assert.equal(balanceBefore.toNumber(), 0);
+
+    // we mint some tokens
+    await smartValorToken.mint.sendTransaction(accountOne, amount);
+
+    // let's get the new balance
+    let balanceAfter = await smartValorToken.balanceOf.call(accounts[1]);
+    assert.equal(balanceBefore.toNumber() + amount, balanceAfter.toNumber());
+  });
+
+  /**
+   * try to mint with a non owner account
+   */
+  it("should's mint any tokens on a non-owner call.", async () => {
+    let falseOwner = accounts[2]; //this account with mint
+    let accountOne = accounts[1]; //to this account
+    let amount = 10000000; //amount of tokens to mint
+
+    // lets get current balance
+    let balanceBefore = await smartValorToken.balanceOf.call(accounts[1]);
+
+    try
+    {
+      // we mint some tokens
+      await smartValorToken.mint.sendTransaction(accountOne, amount, {from: falseOwner});
+    } catch (error)
+    {
+        console.log("Expected error. Trying to mint with a non owner account will fail");
+    }
+
+    // let's get the new balance, should be the same as before
+    let balanceAfter = await smartValorToken.balanceOf.call(accounts[1]);
+    assert.equal(balanceBefore.toNumber(), balanceAfter.toNumber());
+  });
+});
