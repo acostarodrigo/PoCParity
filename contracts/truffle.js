2917a9e5cd19a17fd77b9b2d7a56a27841ff2814
Rodrigo Acosta <rodrigo.acosta@smartvalor.com>
2018-07-01 21:31:40 -0300

    changed configuration file for parity network

--- contracts/truffle.js
+++ contracts/truffle.js
@@ -7,9 +7,9 @@ module.exports = {
   },
   parity: {
     host: "127.0.0.1",
-    port: 8183,
-    network_id: "*", // match any network
-    gas: 0
+    port: 8540,
+    network_id: "parity", // match any network
+    from: "0x004ec07d2329997267Ec62b4166639513386F32E"
   }
 }
 };
