ab27cdb0f14b2ea9a8f0265c5784f36d0bff8961
Rodrigo Acosta <rodrigo.acosta@smartvalor.com>
2018-07-01 21:31:21 -0300

    added a new test

--- contracts/test/SmartValorTokenTests.js
+++ contracts/test/SmartValorTokenTests.js
@@ -11,19 +11,19 @@ contract('MyContract', async (accounts) => {
    * mint some tokens to a non owner account
    */
   it("should mint tokens to a non owner account.", async () => {
-    let owner = accounts[0]; //this account with mint
-    let accountOne = accounts[1]; //to this account
+    let owner = web3.eth.accounts[1]; //this account with mint
+    let accountZero = accounts[0]; //to this account
     let amount = 10000000; //amount of tokens to mint
 
     // account should have no balance
-    let balanceBefore = await smartValorToken.balanceOf.call(accounts[1]);
+    let balanceBefore = await smartValorToken.balanceOf.call(accountZero);
     assert.equal(balanceBefore.toNumber(), 0);
 
     // we mint some tokens
-    await smartValorToken.mint.sendTransaction(accountOne, amount);
+    await smartValorToken.mint.sendTransaction(accountOne, amount, {from: owner});
 
     // let's get the new balance
-    let balanceAfter = await smartValorToken.balanceOf.call(accounts[1]);
+    let balanceAfter = await smartValorToken.balanceOf.call(accountZero);
     assert.equal(balanceBefore.toNumber() + amount, balanceAfter.toNumber());
   });
 
@@ -51,4 +51,22 @@ contract('MyContract', async (accounts) => {
     let balanceAfter = await smartValorToken.balanceOf.call(accounts[1]);
     assert.equal(balanceBefore.toNumber(), balanceAfter.toNumber());
   });
+  /**
+   * mint some tokens to  owner account
+   */
+  it("should mint tokens to the owner.", async () => {
+    let owner = web3.eth.accounts[1]; //this account with mint
+        let amount = 1000000000; //amount of tokens to mint
+
+    // account should have no balance
+    let balanceBefore = await smartValorToken.balanceOf.call(owner);
+
+    // we mint some tokens
+    await smartValorToken.mint.sendTransaction(owner, amount,{from: owner});
+
+    // let's get the new balance
+    let balanceAfter = await smartValorToken.balanceOf.call(owner);
+    assert.equal(balanceBefore.toNumber() + amount, balanceAfter.toNumber());
+  });
+
 });
