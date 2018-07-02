25b206b02926c0eb1ac46238a68c68e80fc2a747
Rodrigo Acosta <rodrigo.acosta@smartvalor.com>
2018-07-01 23:31:28 -0300

    fixed ownership of transactions in test

--- contracts/test/SmartValorTokenTests.js
+++ contracts/test/SmartValorTokenTests.js
@@ -11,19 +11,19 @@ contract('MyContract', async (accounts) => {
    * mint some tokens to a non owner account
    */
   it("should mint tokens to a non owner account.", async () => {
-    let owner = web3.eth.accounts[1]; //this account with mint
-    let accountZero = accounts[0]; //to this account
+    let owner = accounts[0]; //this account with mint
+    let accountOne = accounts[1]; //to this account
     let amount = 10000000; //amount of tokens to mint
 
     // account should have no balance
-    let balanceBefore = await smartValorToken.balanceOf.call(accountZero);
+    let balanceBefore = await smartValorToken.balanceOf.call(accountOne);
     assert.equal(balanceBefore.toNumber(), 0);
 
     // we mint some tokens
     await smartValorToken.mint.sendTransaction(accountOne, amount, {from: owner});
 
     // let's get the new balance
-    let balanceAfter = await smartValorToken.balanceOf.call(accountZero);
+    let balanceAfter = await smartValorToken.balanceOf.call(accountOne);
     assert.equal(balanceBefore.toNumber() + amount, balanceAfter.toNumber());
   });
 
@@ -48,14 +48,14 @@ contract('MyContract', async (accounts) => {
     }
 
     // let's get the new balance, should be the same as before
-    let balanceAfter = await smartValorToken.balanceOf.call(accounts[1]);
+    let balanceAfter = await smartValorToken.balanceOf.call(accountOne);
     assert.equal(balanceBefore.toNumber(), balanceAfter.toNumber());
   });
   /**
    * mint some tokens to  owner account
    */
   it("should mint tokens to the owner.", async () => {
-    let owner = web3.eth.accounts[1]; //this account with mint
+    let owner = accounts[0]; //this account with mint
         let amount = 1000000000; //amount of tokens to mint
 
     // account should have no balance
